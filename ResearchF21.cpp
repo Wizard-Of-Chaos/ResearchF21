cdc9f5223bc7ca70646d6258977e543acc723d55
Wizard-Of-Chaos <59537242+Wizard-Of-Chaos@users.noreply.github.com>
2021-09-30 10:25:15 -0400

    Added a function to generate a file with rand(), sounds real bad

--- ResearchF21.cpp
+++ ResearchF21.cpp
@@ -31,6 +31,21 @@ void scuff(AudioFile<double>& file) //Guaranteed to make any file sound like hot
     file.setNumChannels(1);
     file.setSampleRate(22050);
 }
+
+AudioFile<double> createRandWave()
+{
+    AudioFile<double> randFile;
+    randFile.setNumChannels(2);
+    randFile.setNumSamplesPerChannel((44100) * 10);
+    const double sampleRate = 44100.0;
+    const double frequencyHz = 440.0;
+    for (int i = 0; i < randFile.getNumSamplesPerChannel(); ++i) {
+        for (int j = 0; j < randFile.getNumChannels(); ++j) {
+            randFile.samples[j][i] = rand() % (int)frequencyHz;
+        }
+    }
+    return randFile; //I just wanted to see how this would sound
+}
 int main()
 {
     std::cout << "Generating a freaky-sounding wave in a .wav file..." << std::endl;
@@ -51,6 +66,9 @@ int main()
     loadedAudio.printSummary();
 
     loadedAudio.save("scuffed.wav"); //Implicitly says it's a wav file
+
+    AudioFile<double> randAudio = createRandWave();
+    randAudio.save("rand.wav");
 
     return 0;
 }
